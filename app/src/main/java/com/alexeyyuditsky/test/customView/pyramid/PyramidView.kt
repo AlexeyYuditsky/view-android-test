9918cfdabc2a8321bddb43773fd80c5189c19ae7
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2024-04-23 10:24:30 +0300

    PyramidView update

--- app/src/main/java/com/alexeyyuditsky/test/customView/pyramid/PyramidView.kt
+++ app/src/main/java/com/alexeyyuditsky/test/customView/pyramid/PyramidView.kt
@@ -24,30 +24,26 @@ class PyramidView @JvmOverloads constructor(
         strokeWidth = 12f
     }
 
-    init {
-        invalidate()
-    }
-
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        val firstX = 0.1f * width
-        val firstY = 0.85f * height
+        val leftX = 0.1f * width
+        val leftY = 0.85f * height
 
-        val secondX = 0.9f * width
-        val secondY = 0.85f * height
+        val rightX = 0.9f * width
+        val rightY = 0.85f * height
 
-        val thirdX = 0.4f * width
-        val thirdY = 0.95f * height
+        val bottomX = 0.45f * width
+        val bottomY = 0.95f * height
 
-        val peekX = 0.5f * width
-        val peekY = 0.1f * height
+        val topX = 0.5f * width
+        val topY = 0.1f * height
 
-        canvas.drawLine(firstX, firstY, secondX, secondY, basePaint)
-        canvas.drawLine(firstX, firstY, thirdX, thirdY, basePaint)
-        canvas.drawLine(secondX, secondY, thirdX, thirdY, basePaint)
-        canvas.drawLine(peekX, peekY, firstX, firstY, peekPaint)
-        canvas.drawLine(peekX, peekY, secondX, secondY, peekPaint)
-        canvas.drawLine(peekX, peekY, thirdX, thirdY, peekPaint)
+        canvas.drawLine(leftX, leftY, rightX, rightY, basePaint)
+        canvas.drawLine(leftX, leftY, bottomX, bottomY, basePaint)
+        canvas.drawLine(rightX, rightY, bottomX, bottomY, basePaint)
+        canvas.drawLine(leftX, leftY, topX, topY, peekPaint)
+        canvas.drawLine(rightX, rightY, topX, topY, peekPaint)
+        canvas.drawLine(bottomX, bottomY, topX, topY, peekPaint)
     }
 }
--- app/src/main/java/com/alexeyyuditsky/test/customView/pyramid/PyramidViewFragment.kt
+++ app/src/main/java/com/alexeyyuditsky/test/customView/pyramid/PyramidViewFragment.kt
@@ -1,6 +1,5 @@
 package com.alexeyyuditsky.test.customView.pyramid
 
-import android.os.Bundle
 import android.view.View
 import com.alexeyyuditsky.test.R
 import com.alexeyyuditsky.test.core.AbstractFragment
@@ -10,7 +9,4 @@ class PyramidViewFragment : AbstractFragment<FragmentPyramidBinding>(R.layout.fr
 
     override fun bind(view: View) = FragmentPyramidBinding.bind(view)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
 }
