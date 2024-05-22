2ab98671ffd762e30eb3cda40f2fc93627146361
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2024-05-22 20:10:14 +0300

    New tests

--- app/src/main/java/com/alexeyyuditsky/test/test/Test21.kt
+++ app/src/main/java/com/alexeyyuditsky/test/test/Test21.kt
@@ -1,5 +1,42 @@
 package com.alexeyyuditsky.test.test
 
-fun main() {
+data class Organization(
+    val name: String,
+    val selectedTextIndexes: Pair<Int, Int> = 0 to 0
+)
 
+fun findMatchIndices(text: String, query: String): Pair<Int, Int> {
+    var currentIndex = 0
+
+    text.split(" ").forEach { word ->
+        // Проверяем, если слово начинается с query
+        if (word.startsWith(query, true)) {
+            val startIndex = currentIndex
+            val endIndex = currentIndex + query.length - 1
+            return Pair(startIndex, endIndex)
+        }
+        // Обновляем текущий индекс для следующего слова
+        currentIndex += word.length + 1 // +1 для учета пробела между словами
+    }
+
+    return Pair(0, 0)
+}
+
+fun main() {
+    val organizations =
+        listOf(Organization("ип иванов"), Organization("ИП янде"), Organization("Яндекс маркет"), Organization("мар янд"))
+
+    val result = organizations.map {
+        val selectedTextIndexes = findMatchIndices(it.name, "янд")
+        if (selectedTextIndexes == Pair(0, 0)) it
+        else it.copy(selectedTextIndexes = selectedTextIndexes)
+    }
+
+    val nonZeroSecond = result.filter { it.selectedTextIndexes.second != 0 }.sortedBy { it.selectedTextIndexes.first }
+    val zeroSecond = result.filter { it.selectedTextIndexes.second == 0 }
+
+    // Объединяем отфильтрованные и отсортированные списки
+    val result2 = nonZeroSecond + zeroSecond
+
+    println(result2.map { it.selectedTextIndexes })
 }
---
+++ app/src/main/java/com/alexeyyuditsky/test/test/Test23.kt
@@ -0,0 +1,5 @@
+package com.alexeyyuditsky.test.test
+
+fun main() {
+
+}
