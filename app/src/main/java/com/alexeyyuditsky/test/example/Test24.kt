21ab3a3ad3fc7f9f39dd1bc36db13b8ce0a1ee20
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2024-06-24 17:41:07 +0300

    Refactoring

--- app/src/main/java/com/alexeyyuditsky/test/example/Test24.kt
+++ app/src/main/java/com/alexeyyuditsky/test/example/Test24.kt
@@ -1,21 +1,6 @@
 package com.alexeyyuditsky.test.example
 
-// [-2, 1, 4] -> [1, 4, 16]
 fun main() {
-    val startValues = intArrayOf(-2, 1, 4)
-    val finishValues = getSquareValues(startValues)
-    finishValues.forEach {
-        println(it)
-    }
-}
-
-fun getSquareValues(array: IntArray): IntArray {
-    val newArray = IntArray(array.size)
-
-    array.forEach {
-        val square = it * it
-        
-    }
-
-    return newArray
+    val russianCharSequence = (1072 .. 1103).map { it.toChar() }
+    println(russianCharSequence)
 }
