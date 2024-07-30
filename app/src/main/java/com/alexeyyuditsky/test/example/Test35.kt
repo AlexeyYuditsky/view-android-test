84425254e3e08c01e3e787f9ec8c871b127ee5a9
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2024-07-30 13:28:04 +0300

    Example operator catch for flow

--- app/src/main/java/com/alexeyyuditsky/test/example/Test35.kt
+++ app/src/main/java/com/alexeyyuditsky/test/example/Test35.kt
@@ -1,20 +1,40 @@
 package com.alexeyyuditsky.test.example
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.retry
 
 suspend fun main() {
-    flow.collect {
-        println(it)
-    }
+    flow
+        .map { State.Content(it) as State<*> }
+        .onStart { emit(State.Loading) }
+        .retry(2) { delay(1000); true }
+        .catch { emit(State.Error(it)) }
+        .collect { println(it.returnData()) }
 }
 
 val flow = flow {
-    try {
-        repeat(5) {
-            emit(it)
-        }
-        throw RuntimeException()
-    } catch (e: Exception) {
+    repeat(5) {
+        delay(500)
+        emit(it)
+    }
+    throw RuntimeException()
+}
 
+interface State<T> {
+    fun returnData(): T
+    data class Content(val data: Int) : State<Int> {
+        override fun returnData(): Int = data
+    }
+
+    data object Loading : State<Unit> {
+        override fun returnData() {}
+    }
+
+    data class Error(val t: Throwable) : State<Throwable> {
+        override fun returnData(): Throwable = t
     }
 }
