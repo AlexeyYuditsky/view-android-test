09481a888854aadb4a1fa84b20011fd8af1dfa5d
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2024-10-17 10:22:08 +0300

    coroutines test

--- app/src/main/java/com/alexeyyuditsky/test/example/Test50.kt
+++ app/src/main/java/com/alexeyyuditsky/test/example/Test50.kt
@@ -1,13 +1,20 @@
 package com.alexeyyuditsky.test.example
 
-class Task(val id: Int, val name: String)
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
-fun main() {
-    val task1 = Task(1, "Alex")
-    val task2 = Task(1, "Alex")
-    val hashSet = HashSet<Task>()
-    hashSet.add(task1)
-    hashSet.add(task2)
+suspend fun main() = runBlocking {
+    coroutineScope {
+        launch {
+            println("aaa")
+            delay(1000)
+            println("bbb")
+        }
 
-    println(hashSet)
+        cancel()
+    }
 }
