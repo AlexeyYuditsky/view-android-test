c098c57ef90a9cc632531325cace722ac323f191
AlexeyYuditsky <alexeyyuditsky@gmail.com>
2025-03-12 09:05:34 +0300

    graphql example

--- app/src/main/java/com/alexeyyuditsky/test/screen/graphql/di/NetworkModule.kt
+++ app/src/main/java/com/alexeyyuditsky/test/screen/graphql/di/NetworkModule.kt
@@ -1,10 +1,14 @@
 package com.alexeyyuditsky.test.screen.graphql.di
 
+import com.alexeyyuditsky.test.core.log
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.network.okHttpClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import javax.inject.Singleton
 
 @Module
@@ -13,9 +17,26 @@ class NetworkModule {
 
     @Provides
     @Singleton
-    fun provideApolloClient(): ApolloClient {
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor { message ->
+            log(message, "Apollo")
+        }.apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        return okHttpClient
+    }
+
+    @Provides
+    @Singleton
+    fun provideApolloClient(okHttpClient: OkHttpClient): ApolloClient {
         return ApolloClient.Builder()
             .serverUrl("https://countries.trevorblades.com/graphql")
+            .okHttpClient(okHttpClient)
             .build()
     }
 
--- app/src/main/java/com/alexeyyuditsky/test/screen/graphql/ui/GraphQLFragment.kt
+++ app/src/main/java/com/alexeyyuditsky/test/screen/graphql/ui/GraphQLFragment.kt
@@ -1,14 +1,20 @@
 package com.alexeyyuditsky.test.screen.graphql.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.alexeyyuditsky.test.R
 import com.alexeyyuditsky.test.core.AbstractFragment
+import com.alexeyyuditsky.test.databinding.DialogCountryBinding
 import com.alexeyyuditsky.test.databinding.FragmentGraphqlBinding
+import com.alexeyyuditsky.test.screen.graphql.ui.data.CountriesState
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -20,28 +26,43 @@ class GraphQLFragment :
 
     private val viewModel by viewModels<CountriesViewModel>()
 
+    private lateinit var countryAdapter: CountryAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val countryAdapter = CountryAdapter(viewModel::selectCountry)
-
+        countryAdapter = CountryAdapter(viewModel::selectCountry)
         binding.recyclerView.adapter = countryAdapter
         binding.recyclerView.itemAnimator = null
 
-        lifecycleScope.launch {
-            viewModel.state.flowWithLifecycle(lifecycle).collect {
-                if (it.selectedCountry != null) {
-                    parentFragmentManager
-                        .beginTransaction()
-                        .replace(
-                            R.id.fragmentContainer,
-                            CountryDialog.createCountryDialog(it.selectedCountry)
-                        )
-                        .commit()
-                }
-                countryAdapter.submitList(it.countries)
-                binding.progressBar.isVisible = it.isLoading
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.state.collect(::observeCountriesState)
             }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun observeCountriesState(countriesState: CountriesState) {
+        if (countriesState.selectedCountry != null) {
+            val binding = DialogCountryBinding.inflate(LayoutInflater.from(context))
+            val dialog = AlertDialog.Builder(requireContext())
+                .setView(binding.root)
+                .create()
+
+            with(binding) {
+                flagTextView.text = countriesState.selectedCountry.emoji
+                countryTextView.text = countriesState.selectedCountry.name
+                continentTextView.text = "Continent: " + countriesState.selectedCountry.continent
+                capitalTextView.text = "Capital: " + countriesState.selectedCountry.capital
+                currencyTextView.text = "Currency: " + countriesState.selectedCountry.currency
+                languageTextView.text =
+                    "Language: " + countriesState.selectedCountry.languages.joinToString()
+            }
+
+            dialog.show()
+        } else {
+            countryAdapter.submitList(countriesState.countries)
+            binding.progressBar.isVisible = countriesState.isLoading
         }
     }
 
